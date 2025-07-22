bfeabe3a0e9b81fc5e6128b4deb91d5b03a11ec0
codercoding-code <opo725239@gmail.com>
2025-07-22 14:58:30 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Hello from React!</h1>
+      <p>This is a basic deployed Vite + React site.</p>
+    </div>
+  );
+};
+
+export default App;
